7312ed67d4e01f6ae97a4f5f22ad09d6a74faecb
MatthewAraujo <matthewaraujo20@gmail.com>
2024-09-06 16:45:34 -0300

    ok:wq

--- src/infra/cryptography/bcrypt-hasher.ts
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -6,7 +6,7 @@ export class BcryptHasher implements HasherGenerator, HasherCompare {
 	private HASH_SALT_LENGTH = 8
 
 	hash(plain: string): Promise<string> {
-		return hash(plain, 8)
+		return hash(plain, this.HASH_SALT_LENGTH)
 	}
 
 	compare(plain: string, hash: string): Promise<boolean> {
